e6818a13c4ef92e779cc9ed0aed439230494dc78
SpecSach <romakarpow4@gmail.com>
2026-06-24 15:52:23 +0300

    Added protection against XSS attacks

--- src/entities/todo/ui/TodoItem/TodoItem.jsx
+++ src/entities/todo/ui/TodoItem/TodoItem.jsx
@@ -2,6 +2,7 @@ import { memo, useContext } from "react";
 import { TasksContext } from "@/entities/todo";
 import RouterLink from "@/shared/ui/RouterLink";
 import styles from "./TodoItem.module.scss";
+import { highlightCaseInsensitive } from "../../../../shared/utils/highlight";
 
 const TodoItem = (props) => {
   const { className = "", id, title, isDone } = props;
@@ -13,7 +14,10 @@ const TodoItem = (props) => {
     toggleTaskComplete,
     disappearingTaskId,
     appearingTaskId,
+    searchQuery,
   } = useContext(TasksContext);
+
+  const highlightedTitle = highlightCaseInsensitive(title, searchQuery);
 
   return (
     <li
@@ -33,7 +37,7 @@ const TodoItem = (props) => {
         {title}
       </label>
       <RouterLink to={`/tasks/${id}`} aria-label="Task detail page">
-        {title}
+        <span dangerouslySetInnerHTML={{ __html: highlightedTitle }}></span>
       </RouterLink>
       <button
         className={styles.deleteButton}
